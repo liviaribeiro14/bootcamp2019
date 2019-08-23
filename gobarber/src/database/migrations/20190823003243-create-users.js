9ad0c4e802302c116a5833e79d0fa5babfc46023
Lívia Ribeiro <liviaribeiro14@gmail.com>
2019-08-23 11:48:39 -0300

    feat: User Controller create

--- gobarber/src/database/migrations/20190823003243-create-users.js
+++ gobarber/src/database/migrations/20190823003243-create-users.js
@@ -2,16 +2,43 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+      return queryInterface.createTable('users', {
+        id: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+        },
+        name: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        email: {
+          type: Sequelize.STRING,
+          allowNull: false,
+          unique: true
+        },
+        password_hash: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        provider: {
+          type: Sequelize.BOOLEAN,
+          defaultValue: false,
+          allowNull: false,
+        },
+        created_at: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+        updated_at: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        }
+      });
   },
 
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
+  down: (queryInterface) => {
       return queryInterface.dropTable('users');
-    */
   }
 };
