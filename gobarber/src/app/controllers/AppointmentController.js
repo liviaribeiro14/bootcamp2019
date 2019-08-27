2a7cb4a0a631d73eed8651b28bd3901a8a15bc54
Lívia Ribeiro <liviaribeiro14@gmail.com>
2019-08-27 17:38:39 -0300

    feat: Queue with Redis

--- gobarber/src/app/controllers/AppointmentController.js
+++ gobarber/src/app/controllers/AppointmentController.js
@@ -2,11 +2,12 @@ import * as Yup from 'yup';
 import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import pt from 'date-fns/locale/pt';
 
-import Mail from '../../lib/Mail';
 import Notification from '../schemas/Notification';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
+import Queue from '../../lib/Queue';
+import CancellationMail from '../jobs/CancellationMail';
 
 class AppointmentController{
   async index(req, res){
@@ -144,21 +145,12 @@ class AppointmentController{
 
     appointment.canceled_at = new Date();
 
-    //await appointment.save();
+    await appointment.save();
 
-    await Mail.sendMail({
-      to: `${appointment.provider.name} <${appointment.provider.email}>`,
-      subject: 'Agendamento cancelado',
-      template: 'cancellation',
-      context: {
-        provider: appointment.provider.name,
-        user: appointment.user.name,
-        date: format(appointment.date,
-          "'dia' dd 'de' MMMM', às' H:mm'h'",
-          { locale: pt }
-        ),
-      }
+    await Queue.add(CancellationMail.key, {
+      appointment,
     });
+
     return res.json(appointment);
   }
 }
