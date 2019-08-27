278a64cc2a6c96475c41e98af96b33ff5a01eb1a
Lívia Ribeiro <liviaribeiro14@gmail.com>
2019-08-27 16:01:52 -0300

    feat: Cancellation appointment email

--- gobarber/src/app/controllers/AppointmentController.js
+++ gobarber/src/app/controllers/AppointmentController.js
@@ -1,6 +1,8 @@
 import * as Yup from 'yup';
 import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import pt from 'date-fns/locale/pt';
+
+import Mail from '../../lib/Mail';
 import Notification from '../schemas/Notification';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
@@ -112,7 +114,23 @@ class AppointmentController{
   }
 
   async delete(req, res){
-    const appointment = await Appointment.findByPk(req.params.id);
+    const appointment = await Appointment.findByPk(
+      req.params.id,
+      {
+        include: [
+          {
+            model: User,
+            as: 'provider',
+            attributes: ['name', 'email'],
+          },
+          {
+            model: User,
+            as: 'user',
+            attributes: ['name'],
+          }
+        ]
+      }
+    );
 
     if(appointment.user_id !== req.userId){
       return res.status(401).json('You do not have permission to cancel this appointment.')
@@ -126,8 +144,21 @@ class AppointmentController{
 
     appointment.canceled_at = new Date();
 
-    await appointment.save();
+    //await appointment.save();
 
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Agendamento cancelado',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(appointment.date,
+          "'dia' dd 'de' MMMM', às' H:mm'h'",
+          { locale: pt }
+        ),
+      }
+    });
     return res.json(appointment);
   }
 }
---
+++ gobarber/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  secure: false,
+  auth: {
+    user: "8e7778033e03f8",
+    pass: "6d4e7619f4a581"
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  }
+};
