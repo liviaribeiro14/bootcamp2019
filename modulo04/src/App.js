671814f0f16f7b94ae2e57bcc6af3c8d3a55c160
Livia <liviaribeiro14@gmail.com>
2019-09-01 12:53:01 -0300

    Modulo04

---
+++ modulo04/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+import TechList from "./components/TechList";
+import profile from "./assets/livia_avatar.png";
+
+function App() {
+  //return <h1>Hello Rocketseat</h1>;
+  //return <img src={profile} />;
+  return <TechList />;
+}
+
+export default App;
