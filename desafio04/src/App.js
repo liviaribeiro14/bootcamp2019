216b8098111e9df8395004a151bdd00f556625ab
Livia <liviaribeiro14@gmail.com>
2019-09-01 18:08:13 -0300

    feat: initial configuration

---
+++ desafio04/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+import Facebook from './components/Facebook';
+
+function App() {
+  return <Facebook />;
+}
+
+export default App;
---
+++ desafio04/src/components/Facebook.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Facebook extends Component {
+  render() {
+    return (
+      <div className="header">hello</div>
+    );
+  }
+}
+
+export default Facebook;
